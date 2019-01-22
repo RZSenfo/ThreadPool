bc4c2fcd49330ed98fc95e5f09867185078c7689
RZSenfo <lpsenfo@web.de>
2019-01-22 12:19:47 +0100

    [unverified] multiple worker queues
    to disable mutex fighting

--- ThreadPool.hpp
+++ ThreadPool.hpp
@@ -6,55 +6,80 @@
 #include <queue>
 
 class ThreadPool {
+private:
+    
+    struct WorkerQueue {
+        // the task queue
+        std::queue< std::packaged_task<void()> > tasks;
+        
+        // synchronization
+        std::mutex queue_mutex;
+        std::condition_variable condition;
+    };
+    
+    // need to keep track of threads so we can join them
+    std::vector< std::thread > workers;
+    
+    // queues
+    std::vector< WorkerQueue > queues;
+    
+    bool stop;
+    
+    std::atomic<size_t> round_robin_idx = 0;
+    
 public:
-
     // the constructor just launches some amount of workers
     explicit ThreadPool(size_t) : stop(false)
     {
-        for(size_t i = 0;i<threads;++i)
-            workers.emplace_back(
-                [this]
+        for(size_t i = 0;i<threads;++i) {
+            this->queues.emplace_back(
+                WorkerQueue()
+            );
+            this->workers.emplace_back(
+                [this,i]
                 {
                     for(;;)
                     {
                         std::packaged_task<void()> task;
 
                         {
-                            std::unique_lock<std::mutex> lock(this->queue_mutex);
-                            this->condition.wait(lock,
-                                [this]{ return this->stop || !this->tasks.empty(); });
-                            if(this->stop && this->tasks.empty())
+                            std::unique_lock<std::mutex> lock(this->queues[i].queue_mutex);
+                            this->queues[i].condition.wait(lock,
+                                [this]{ return this->stop || !this->queues[i].tasks.empty(); });
+                            if(this->stop && this->queues[i].tasks.empty())
                                 return;
-                            task = std::move(this->tasks.front());
-                            this->tasks.pop();
+                            task = std::move(this->queues[i].tasks.front());
+                            this->queues[i].tasks.pop();
                         }
 
                         task();
                     }
                 }
             );
+        }
     }
 
     // stop the pool before deleting
     ~ThreadPool()
     {
-        stopThreadPool(true);
+        this->stopThreadPool(true);
     }
 
     // stop the pool
     void stopThreadPool(bool finishTasks = false)
     {
-        if (!stop) {
-            {
-                std::unique_lock<std::mutex> lock(queue_mutex);
-                stop = true;
-
-                if (!finishTasks) {
-                    std::queue< std::packaged_task<void()> > empty;
-                    std::swap(tasks, empty);
+        if (!this->stop) {
+            this->stop = true;
+            for (size_t i = 0; i < this->queues.size(); i++) {
+                {
+                    std::unique_lock<std::mutex> lock(this->queues[i].queue_mutex);
+                    if (!finishTasks) {
+                        std::queue< std::packaged_task<void()> > empty;
+                        std::swap(this->queues[i].tasks, empty);
+                    }
                 }
+                this->queues[i].condition.notify_all();
             }
-            condition.notify_all();
         }
 
         for (auto& x : workers) {
@@ -70,32 +95,23 @@ public:
         using return_type = std::invoke_result_t<F, Args...>;
 
         std::packaged_task<return_type()> task(
-                std::bind(std::forward<F>(f), std::forward<Args>(args)...)
-            );
+            std::bind(std::forward<F>(f), std::forward<Args>(args)...)
+        );
+        
+        size_t chosen_worker = (++this->round_robin_idx >= this->workers.size()) ? (this->round_robin_idx = 0) : this->round_robin_idx)
 
         std::future<return_type> res = task.get_future();
         {
-            std::unique_lock<std::mutex> lock(queue_mutex);
+            std::unique_lock<std::mutex> lock(this->queues[chosen_worker].queue_mutex);
 
             // don't allow enqueueing after stopping the pool
             if(stop)
                 throw std::runtime_error("enqueue on stopped ThreadPool");
 
-            tasks.emplace(std::move(task));
+            this->queues[chosen_worker].tasks.emplace(std::move(task));
         }
-        condition.notify_one();
+        this->queues[chosen_worker].condition.notify_one();
         return res;
     }
-
-private:
-    // need to keep track of threads so we can join them
-    std::vector< std::thread > workers;
-    // the task queue
-    std::queue< std::packaged_task<void()> > tasks;
-
-    // synchronization
-    std::mutex queue_mutex;
-    std::condition_variable condition;
-    bool stop;
 };
 #endif
